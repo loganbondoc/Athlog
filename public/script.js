2e8ab81104204aa6e5296a8a961d7303c519e3e1
Logan Bondoc <loganbondoc@gmail.com>
2023-06-07 01:26:01 +1000

    implemented timer countdown, timer now changes based on intensity

--- public/script.js
+++ public/script.js
@@ -9,6 +9,7 @@ const repCounter = document.getElementById("rep-counter");
 var sessions = [];
 localStorage.setItem('numSessions', '0');
 localStorage.setItem('sessionInProgress', 'false');
+localStorage.setItem('timerRunning', 'false');
 localStorage.setItem('currentExerciseIndex', '0');
 localStorage.setItem('sessions', '');
 localStorage.setItem('numReps', '0');
@@ -73,17 +74,17 @@ class Session {
             break;
         }
       
-        // switch(currentSession.intensity){
-        //   case "Light":
-        //     var categoryNames = pushArms;
-        //     break;
-        //   case "Moderate":
-        //     var categoryNames = pullArms;
-        //     break;
-        //   case "Intense":
-        //     var categoryNames = pushLegs;
-        //     break;
-        // }
+        switch(this.intensity){
+          case "Light":
+            this.restMinutes = 4;
+            break;
+          case "Moderate":
+            this.restMinutes = 3;
+            break;
+          case "Intense":
+            this.restMinutes = 2;
+            break;
+        }
     }
 }
 
@@ -189,6 +190,8 @@ function generateSession(event) {
     repTable.appendChild(row);
   }
 
+  // setting default timer
+  document.getElementById("timer-time").innerHTML = "0" + currentSession.restMinutes + ":00";
 }
 
 // Hides tracker page based on is a session is in progress or not
@@ -206,6 +209,7 @@ function changeSessionPage(){
 
 repCounter.addEventListener("submit", recordReps);
 repCounter.addEventListener("submit", checkSession);
+repCounter.addEventListener("submit", startRest);
 
 // check if session is finished
 function checkSession(){
@@ -263,3 +267,45 @@ function recordReps(event){
 
   console.log(currentSession.exercises);
 }
+
+// start rest timer countdown
+function startRest(event){
+  event.preventDefault();
+
+  localStorage.setItem('timerRunning', 'true');
+
+  // Following code adapted from https://www.w3schools.com/howto/howto_js_countdown.asp
+  // Set the date we're counting down to
+  let currentSession = sessions[sessions.length - 1];
+  var startTime = new Date();
+  var targettedTime = startTime.setMinutes(startTime.getMinutes() + currentSession.restMinutes);
+
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = targettedTime - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    if (seconds.length == 1){
+      seconds = "0" + seconds;
+    }
+
+    // Display the result in the element with id="timer-time"
+    document.getElementById("timer-time").innerHTML = "0" + minutes + ":" + seconds;
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("timer-time").innerHTML = "--:--";
+      localStorage.setItem('timerRunning', 'false');
+    }
+  }, 1000);
+  }
